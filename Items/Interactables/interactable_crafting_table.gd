f6ad161fb0b951d8269dae47502393c68a8de67f
August13742 <149452958+August13742@users.noreply.github.com>
2025-05-29 14:27:29 +0900

    adjusted crafting table implementation

--- Items/Interactables/interactable_crafting_table.gd
+++ Items/Interactables/interactable_crafting_table.gd
@@ -1,14 +1,10 @@
-extends Node3D
+extends Interactable
 
 class_name CraftingTable
 
 
 @export var crafting_menu_ui = preload("uid://c1w6mvkqn1h1h")
 
-func _ready():
-	EventSystem.INT_begin_interaction.connect(_on_interact)
-
-
-func _on_interact():
+func start_interaction():
 	var crafting_menu:Control = crafting_menu_ui.instantiate()
 	get_tree().get_first_node_in_group("ui_layer").add_child(crafting_menu)
