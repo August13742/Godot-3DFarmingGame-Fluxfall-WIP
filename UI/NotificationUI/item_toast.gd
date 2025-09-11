6aa7980f8199f82d5ca3db9df88a6ed34e9c6fa3
August13742 <149452958+August13742@users.noreply.github.com>
2025-09-11 14:01:31 +0900

    slight toaster tweaks

--- UI/NotificationUI/item_toast.gd
+++ UI/NotificationUI/item_toast.gd
@@ -19,14 +19,14 @@ func start(text:String, icon:Texture2D, hold_s:float):
 func _play(in_d:float, hold:float, out_d:float) -> void:
 	modulate.a = 0.0
 	position.x += 16
-	var t = get_tree().create_tween();
+	var t = get_tree().create_tween(); t.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
 	t.tween_property(self, "modulate:a", 1.0, in_d).set_trans(Tween.TRANS_QUAD)
 	t.parallel().tween_property(self, "position:x", position.x - 16, in_d)
 	await t.finished
 	if not _alive: return
-	await get_tree().create_timer(hold).timeout
+	await get_tree().create_timer(hold,true).timeout
 	if not _alive: return
-	var t2 = get_tree().create_tween();
+	var t2 = get_tree().create_tween(); t2.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
 	t2.tween_property(self, "modulate:a", 0.0, out_d).set_trans(Tween.TRANS_QUAD)
 	await t2.finished
 	_alive = false
