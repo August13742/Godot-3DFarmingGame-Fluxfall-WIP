f35fb2b5b2d81979f9cb69e4c3c3f13e324c622e
August13742 <149452958+August13742@users.noreply.github.com>
2025-09-14 23:34:39 +0900

    temp commit to port item system as submodule

--- addons/augusts_item_system/actionable_capability.gd
+++
@@ -1,6 +0,0 @@
-extends ItemCapability
-class_name ActionableCapability
-
-# This class is intentionally blank.
-# Any capability that requires a handler to execute its logic should inherit
-# from this class instead of ItemCapability directly.
--- addons/augusts_item_system/capability_handler.gd
+++
@@ -1,5 +0,0 @@
-extends RefCounted
-class_name CapabilityHandler
-
-func execute(target: Node, capability_data: ItemCapability):
-	push_error("The 'execute' method must be overridden in the concrete handler: %s" % get_script().resource_path)
