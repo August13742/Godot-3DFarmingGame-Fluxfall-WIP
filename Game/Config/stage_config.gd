bab468e4337c46e2c7c1bdc6150660d966c9d043
August13742 <149452958+August13742@users.noreply.github.com>
2025-09-20 13:01:48 +0900

    using path instead of UID for key areas to avoid crash

--- Game/Config/stage_config.gd
+++ Game/Config/stage_config.gd
@@ -7,7 +7,7 @@ enum Keys {
 
 const STAGE_PATH:= {
 	Keys.Island:"uid://37ea3gb4pcdi",
-	Keys.Test:"uid://dptyxxs3uol81"
+	Keys.Test:"res://Stages/props_display_clean.tscn"
 }
 
 static func get_stage(key:Keys)->Node:
